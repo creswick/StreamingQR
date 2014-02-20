3ceef4b9922cbd442f53f1ba489eaa82f4e8a47f
Don Park <donp@donp.org>
2014-02-20 11:15:37 -0800

    unit test bits

---
+++ development/android/qrstream/src/instrumentTest/java/com/galois/qrstream/test/PreviewTest.java
@@ -0,0 +1,12 @@
+package com.galois.qrstream.test;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by donp on 2/20/14.
+ */
+public class PreviewTest extends InstrumentationTestCase {
+    public void test() throws Exception {
+        assertEquals(true, true);
+    }
+}
