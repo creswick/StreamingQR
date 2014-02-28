4cef961df7945d80ee22bdd91f2e57e3f8857c43
Don Park <donp@donp.org>
2014-02-28 11:29:31 -0800

    rework the Receive method to put frames into a queue

--- development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/Preview.java
+++ development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/Preview.java
@@ -1,23 +1,31 @@
 package com.galois.qrstream.lib;
 
+
 import android.hardware.Camera;
 import android.util.Log;
 
-import com.galois.qrstream.qrpipe.Receive;
+import com.galois.qrstream.image.YuvImage;
+
+import java.util.Queue;
 
 /**
  * Created by donp on 2/13/14.
  */
 public class Preview implements Camera.PreviewCallback {
-    Receive qrpipe;
+    Queue frames;
+    int height;
+    int width;
 
-    public void setQrpipe(Receive qrpipe) {
-        this.qrpipe = qrpipe;
+    public void setQueue(Queue frames) {
+        this.frames = frames;
     }
+    public void setHeight(int h) { this.height = h; }
+    public void setWidth(int w) { this.width = w; }
 
     @Override
     public void onPreviewFrame(byte[] data, Camera camera) {
         Log.d("qrstream", "previewFrame data "+data.length);
-        // qrpipe.decodeQRCodes(null); #Function not yet implemented
+        YuvImage frame = new YuvImage(data, height, width);
+        frames.add(frame);
     }
 }
--- development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/QrpipeFragment.java
+++ development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/QrpipeFragment.java
@@ -2,18 +2,24 @@ package com.galois.qrstream.lib;
 
 import android.app.Fragment;
 
+import com.galois.qrstream.image.YuvImage;
 import com.galois.qrstream.qrpipe.Receive;
+
+import java.util.Iterator;
+import java.util.concurrent.ArrayBlockingQueue;
 
 /**
  * Created by donp on 2/24/14.
  */
 public class QrpipeFragment extends Fragment {
-    Receive qrpipe;
+    Receive receiveQrpipe;
+    ArrayBlockingQueue frameQueue;
 
     @Override
     public void onStart() {
         super.onStart();
-        qrpipe = new Receive(640, 480);
+        receiveQrpipe = new Receive(640, 480);
+        frameQueue = new ArrayBlockingQueue<YuvImage>(1);
     }
 
 }
