b602c0e3c5a995a888e586df8c17fc29ff96b584
Don Park <donp@donp.org>
2014-03-03 10:50:20 -0800

    first tech for qrstreamlib

---
+++ development/android/qrstreamlib/src/instrumentTest/java/com/galois/qrstream/lib/test/FirstTest.java
@@ -0,0 +1,12 @@
+package com.galois.qrstream.lib.test;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by donp on 3/3/14.
+ */
+public class FirstTest extends InstrumentationTestCase {
+    public void test() throws Exception {
+        assertEquals(true, true);
+    }
+}
