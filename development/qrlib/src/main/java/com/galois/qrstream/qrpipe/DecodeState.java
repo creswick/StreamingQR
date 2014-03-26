9ff4ccd5d5b9f5796f49813f1c3732fac301e38d
Ledah Casburn <lcasburn@galois.com>
2014-03-26 09:58:30 -0700

    qrlib: Require number of QR codes to be known before initializing DecodeState. qrlib: Added failed transmission state

--- development/qrlib/src/main/java/com/galois/qrstream/qrpipe/DecodeState.java
+++ development/qrlib/src/main/java/com/galois/qrstream/qrpipe/DecodeState.java
@@ -7,24 +7,26 @@ import java.util.BitSet;
  */
 public class DecodeState {
 
-	private BitSet data = null;
-	private int capacity = 0;
+	private final BitSet data;
+	private final int capacity;
+
+	// True if transmission of QR codes stops before entire message received
+	private boolean hasTransmissionFailed;
 
 	/**
-	 * Initialize DecodeState with given capacity. After initialization, requests
-	 * to change capacity are ignored. This allows external applications to query
-	 * status of transmission and QR decoding, in the case that {@code Receive}
-	 * is has not yet received and decoded its first QR code. It needs at least
-	 * one QR code to know how many QR codes it should expect to receive.
+	 * Initialize DecodeState with the number of QR codes that it expects
+	 * in the stream. The caller should have decoded at least one QR code to
+	 * know how many QR codes it should expect to receive and pass that
+	 * number to this constructor.
+	 * @param capacity The number of QR code chunks in a full message.
 	 */
-	public void setInitialCapacity(int capacity) {
-		if (this.data == null) {
-			if (capacity < 0) {
-				throw new IllegalArgumentException("DecodeState must have capacity > 0");
-			}
-			this.capacity = capacity;
-			this.data = new BitSet(capacity);
+	public DecodeState(int capacity) {
+		if (capacity < 0) {
+			throw new IllegalArgumentException("DecodeState must have capacity > 0");
 		}
+		this.capacity = capacity;
+		this.data = new BitSet(capacity);
+		this.hasTransmissionFailed = false;
 	}
 
 	/**
@@ -35,7 +37,9 @@ public class DecodeState {
 	 * transitions to {@code Final}.
 	 */
 	public State getState () {
-		if (data == null || data.isEmpty()) {
+		if (hasTransmissionFailed) {
+			return State.Fail;
+		} else if (data.isEmpty()) {
 			return State.Initial;
 		} else if (data.cardinality() == this.capacity) {
 			return State.Final;
@@ -52,13 +56,19 @@ public class DecodeState {
 	 * of capacity of underlying bitset.
 	 */
 	public void markDataReceived (int chunkId) throws IndexOutOfBoundsException {
-		if (data != null) {
-			if (chunkId < 1 || chunkId > capacity) {
-				throw new IndexOutOfBoundsException("Cannot mark bit, chunkId: " + chunkId +
-				                                    ", is out of bounds");
-			}
-			data.set(chunkId - 1);
+		if (chunkId < 1 || chunkId > capacity) {
+			throw new IndexOutOfBoundsException("Cannot mark bit the  chunkId: " + chunkId +
+			                                    ", is out of bounds");
 		}
+		data.set(chunkId - 1);
+	}
+
+	/**
+	 * Indicate that transmission failed and we expect no more QR codes
+	 * to decode.
+	 */
+	public void markFailedTransmission() {
+		this.hasTransmissionFailed = true;
 	}
 
 	/**
