7bc5db9351b1dd5f427fefb9489200cd7c9204d4
Don Park <donp@donp.org>
2014-03-28 15:14:52 -0700

    the ENUM is enough

--- development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/Progress.java
+++ development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/Progress.java
@@ -12,10 +12,7 @@ import com.galois.qrstream.qrpipe.State;
  * Created by donp on 3/5/14.
  */
 public class Progress implements IProgress {
-    public final static int INITIAL = 1;
-    public final static int INTERMEDIATE = 2;
-    public final static int FINAL = 3;
-    Handler handler;
+    private Handler handler;
 
     public void setStateHandler(Handler handler){
         this.handler = handler;
