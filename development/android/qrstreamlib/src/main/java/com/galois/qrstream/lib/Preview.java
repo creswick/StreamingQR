d54251b42265fba3989d205141ed8469dd820dda
Don Park <donp@donp.org>
2014-04-15 11:42:19 -0700

    recirculate the frame buffer

--- development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/Preview.java
+++ development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/Preview.java
@@ -32,10 +32,11 @@ public class Preview implements Camera.PreviewCallback {
 
     @Override
     public void onPreviewFrame(byte[] data, Camera camera) {
-        Log.v(Constants.APP_TAG, "previewFrame data "+data.length);
+        Log.v(Constants.APP_TAG, "onPreviewFrame data len "+data.length);
         YuvImage frame = new YuvImage(data, height, width);
         if(frames.offer(frame) == false) {
             Log.v(Constants.APP_TAG, "Frame queue full!");
         }
+        camera.addCallbackBuffer(data);
     }
 }
