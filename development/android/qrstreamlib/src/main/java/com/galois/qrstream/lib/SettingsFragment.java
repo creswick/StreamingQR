c1ea1725b25d62d664c65fc1cf8428a261256bc7
Ledah Casburn <lcasburn@galois.com>
2014-04-30 16:47:49 -0700

    setup default settings and back button to exit from settings fragment

--- development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/SettingsFragment.java
+++ development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/SettingsFragment.java
@@ -1,6 +1,5 @@
 package com.galois.qrstream.lib;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
@@ -8,6 +7,7 @@ import android.preference.PreferenceFragment;
  * Created by donp on 4/8/14.
  */
 public class SettingsFragment extends PreferenceFragment {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
