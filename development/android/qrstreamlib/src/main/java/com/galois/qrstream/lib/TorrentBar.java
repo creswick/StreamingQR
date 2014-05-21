365930c0b70be66149f5af8656def6b7f8428dd6
Don Park <donp@donp.org>
2014-05-21 15:37:39 -0700

    bittorrent-style progress bar

---
+++ development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/TorrentBar.java
@@ -0,0 +1,10 @@
+package com.galois.qrstream.lib;
+
+import android.content.Context;
+import android.view.View;
+
+public class TorrentBar extends View {
+    public TorrentBar(Context context) {
+        super(context);
+    }
+}
