e6a3ab39573e2eafe04cde687029ea7152d25d1b
Don Park <donp@donp.org>
2014-05-21 15:44:38 -0700

    the start of the TorrentBar

--- development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/TorrentBar.java
+++ development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/TorrentBar.java
@@ -1,10 +1,11 @@
 package com.galois.qrstream.lib;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.view.View;
 
 public class TorrentBar extends View {
-    public TorrentBar(Context context) {
-        super(context);
+    public TorrentBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
     }
 }
