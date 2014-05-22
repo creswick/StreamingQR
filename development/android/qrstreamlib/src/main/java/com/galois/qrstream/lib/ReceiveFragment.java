826c8405691d3a8e7b830affcbe1e91e2abbf64e
Don Park <donp@donp.org>
2014-05-22 10:41:27 -0700

    begin the TorrentBar

--- development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/ReceiveFragment.java
+++ development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/ReceiveFragment.java
@@ -18,7 +18,6 @@ package com.galois.qrstream.lib;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.app.Fragment;
 import android.content.DialogInterface;
 import android.hardware.Camera;
@@ -31,9 +30,7 @@ import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
-import android.widget.LinearLayout;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -47,7 +44,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
-import java.util.Calendar;
 import java.util.concurrent.BlockingQueue;
 
 /**
@@ -58,7 +54,7 @@ public class ReceiveFragment extends Fragment implements SurfaceHolder.Callback 
     private SurfaceView camera_window;
     private ViewGroup.LayoutParams camera_window_params;
     private RelativeLayout rootLayout;
-    private ProgressBar progressBar;
+    private TorrentBar torrentBar;
     private TextView progressText;
 
     private Camera camera;
@@ -95,7 +91,7 @@ public class ReceiveFragment extends Fragment implements SurfaceHolder.Callback 
                         int count = params.getInt("chunk_count");
                         int total = params.getInt("chunk_total");
                         Log.d(Constants.APP_TAG, "DisplayUpdate.handleMessage setProgress " + progressStatus);
-                        progressBar.setProgress(progressStatus);
+                        torrentBar.setProgress(progressStatus);
                         progressText.setText(""+count+"/"+total+" "+progressStatus+"%");
                     }
                 });
@@ -105,7 +101,7 @@ public class ReceiveFragment extends Fragment implements SurfaceHolder.Callback 
                 activity.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        progressBar.setProgress(progressBar.getMax());
+                        torrentBar.setProgress(torrentBar.getMax());
                         rootLayout.removeView(camera_window);
                     }
                 });
@@ -158,7 +154,7 @@ public class ReceiveFragment extends Fragment implements SurfaceHolder.Callback 
         /* remember the camera_window details for rebuilding later */
         camera_window_params = camera_window.getLayoutParams();
         setCameraWindowCallback();
-        progressBar = (ProgressBar) rootView.findViewById(R.id.progressbar);
+        torrentBar = (TorrentBar) rootView.findViewById(R.id.progressbar);
         progressText = (TextView) rootView.findViewById(R.id.progresstext);
         return rootView;
     }
@@ -207,7 +203,7 @@ public class ReceiveFragment extends Fragment implements SurfaceHolder.Callback 
      * Reset the UI elements to an initial state.
      */
     private void resetUI() {
-        progressBar.setProgress(0);
+        torrentBar.setProgress(0);
         progressText.setText("");
     }
 
--- development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/TorrentBar.java
+++ development/android/qrstreamlib/src/main/java/com/galois/qrstream/lib/TorrentBar.java
@@ -1,11 +1,50 @@
 package com.galois.qrstream.lib;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class TorrentBar extends View {
+    private Paint mPaint;
+    private RectF mBounds;
+
     public TorrentBar(Context context, AttributeSet attrs) {
         super(context, attrs);
+        paintInit();
+    }
+
+    private void paintInit() {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.BLUE);
+    }
+
+    protected void onSizeChanged (int w, int h, int oldw, int oldh) {
+        Log.d("qrstream", "onSizeChanged w:"+w+" h:"+h+" ow:"+oldw+" oh:"+oldh);
+        mBounds = new RectF();
+        int aspectHeight= w/10;
+        mBounds.set(0,0,w,aspectHeight);
+
+    }
+
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // Draw the shadow
+        canvas.drawOval(
+                mBounds,
+                mPaint
+        );
+    }
+
+    public void setProgress(int progressStatus) {
+    }
+
+    public int getMax() {
+        return 100;
     }
 }
