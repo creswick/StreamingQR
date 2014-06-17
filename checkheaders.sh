706edc94166b8766088be9f0eab07f649e52f18d
Rogan Creswick <creswick@gmail.com>
2014-06-17 16:31:00 -0700

    updated check headers script

--- checkheaders.sh
+++ checkheaders.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 # A very simple check to find files with no copyright headers:
-find . -name "*.java" | xargs grep -L "opyright"
+files=`find . -name "*.java" | xargs grep -L "opyright" | grep -v build/generated`
+
+# If nothing is found, we'll exit with this successful exit code:
+exitCode=0
+
+while read -r file; do
+# If we're in this loop, then the script should exit with an error condition:
+exitCode=1
+echo ${file}
+done <<< "$files"
+
+exit ${exitCode}
+
