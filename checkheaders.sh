7c1254d0cc632310a1bfae939653d856e12a2dfa
Rogan Creswick <creswick@gmail.com>
2014-06-17 16:53:16 -0700

    clarified the output of checkheaders

--- checkheaders.sh
+++ checkheaders.sh
@@ -6,6 +6,7 @@ files=`find . -name "*.java" | xargs grep -L "opyright" | grep -v build/generate
 # If nothing is found, we'll exit with this successful exit code:
 exitCode=0
 
+echo "Checking java files for copyright headers"
 while read -r file; do
 # If we're in this loop, then the script should exit with an error condition:
 exitCode=1
